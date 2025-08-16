99edec5c93e4e9c98fa87f5a5122e6e23558f297
nandini pande <nandinipande27@gmail.com>
2025-08-16 23:19:45 +0530

    first project added

--- one.py
+++ one.py
@@ -1,4 +1,20 @@
-x=12
-y=13
-z=x+y
-print(z)
+import random
+while True :
+ print("😉-----number guessing game----😀")
+ s=int(input("enter the start number :"))
+ e=int(input("enter the end number:"))
+ print(f"your range is {s} to {e} ")
+
+ ran_no=random.randint(s,e)
+
+ while True :
+     x=int(input("Enter your guessing number"))
+     if ran_no ==x :
+      print("you guess the correct ✔")
+      print("you got it 👍")
+      break
+     elif ran_no >x :
+      print(f"the number greater than {x} ")
+     elif ran_no <x :
+      print(f"the number is less than {x} ")
+ 
